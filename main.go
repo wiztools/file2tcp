9010fc39f3df77551a26bdce87cdff9ea16e1391
Subhash Chandran <subhash@striim.com>
2018-09-29 05:08:17 +0530

    added verbose flag; support for reading from the socket introduced

--- main.go
+++ main.go
@@ -9,45 +9,62 @@ import (
 	"time"
 )
 
-func main() {
-	fmt.Println("file2tcp")
+func printErr(msg string) {
+	fmt.Fprintln(os.Stderr, msg)
+}
 
+func readFromServer(conn net.Conn) {
+	rdr := bufio.NewReader(conn)
+	for true {
+		str, err := rdr.ReadString('\n')
+		if err != nil {
+			printErr("Error reading")
+			panic(err)
+		}
+		fmt.Println(str)
+	}
+}
+
+func main() {
 	var (
 		file     string
 		host     string
 		port     uint
 		interval bool
 		crlf     bool
+		verbose  bool
 	)
-	flag.StringVar(&file, "f", "", "File")
-	flag.StringVar(&host, "h", "127.0.0.1", "Hostname")
+	flag.StringVar(&file, "f", "", "File.")
+	flag.StringVar(&host, "h", "127.0.0.1", "Hostname.")
 	flag.UintVar(&port, "p", 0, "Port")
 	flag.BoolVar(&interval, "i", false, "Add interval of 1 sec after every line.")
 	flag.BoolVar(&crlf, "crlf", false, "Pass CRLF as EOL character. Default is LF.")
+	flag.BoolVar(&verbose, "v", false, "Verbose.")
 	flag.Parse()
 
 	// Validations:
 	if file == "" {
-		fmt.Println("No file given.")
+		printErr("No file given.")
 		os.Exit(1)
 	}
 	if port == 0 {
-		fmt.Println("Port is not set.")
+		printErr("Port is not set.")
 		os.Exit(1)
 	}
 
 	// Read and send to socket:
 	conn, err := net.Dial("tcp", fmt.Sprintf("%v:%v", host, port))
 	if err != nil {
-		fmt.Println("Error opening TCP")
+		printErr("Error opening TCP")
 		panic(err)
 	}
 	defer conn.Close()
-	fmt.Println("Conn est")
+
+	go readFromServer(conn)
 
 	fp, err := os.Open(file)
 	if err != nil {
-		fmt.Println("File open error")
+		printErr("File open error")
 		panic(err)
 	}
 	defer fp.Close()
@@ -59,14 +76,16 @@ func main() {
 	}
 	for scanner.Scan() {
 		txt := scanner.Text()
-		fmt.Println(txt)
+		if verbose {
+			fmt.Println(txt)
+		}
 		fmt.Fprint(conn, txt+eol)
 		if interval {
 			time.Sleep(1 * time.Second)
 		}
 	}
 	if err := scanner.Err(); err != nil {
-		fmt.Println("Scanner error")
+		printErr("Scanner error")
 		panic(err)
 	}
 }
